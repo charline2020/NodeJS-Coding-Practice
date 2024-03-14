924cdbe2e5723fe6c3d2dc563d2fe75513b82a1c
charline2020 <p78071044@gs.ncku.edu.tw>
2024-03-14 09:37:13 +0800

    set response message

--- get_http_response.js
+++ get_http_response.js
@@ -5,19 +5,32 @@ const http = require('http');
 const server = http.createServer((request, response) => {    
     
     // get http method
-    console.log(request.method);
+    // console.log(request.method);
 
     // get url
-    console.log(request.url); // only shows url path and search text
+    // console.log(request.url); // only shows url path and search text
 
     // get http request head
-    console.log(request.headers);
+    // console.log(request.headers);
 
     // get host from http request head
-    console.log(request.headers.host);
+    // console.log(request.headers.host);
+
+    // set response status
+    response.statusCode = 200;
+    
+    // set response message
+    response.statusMessage = 'OK';
+
+    // set response header
+    response.setHeader('content-type', 'text/html;charset=utf-8')
+
+    // set response body
+    response.write('hello <br/>');
+    response.write('this is main page');
 
     // show response body
-    response.end('hello http server');
+    response.end();
 
 });
 
