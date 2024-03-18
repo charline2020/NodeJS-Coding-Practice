0de5a1ea31f6fa1ccdd3638dda0179f948826b41
charline2020 <p78071044@gs.ncku.edu.tw>
2024-03-18 08:58:11 +0800

    build static server

--- static_server/server.js
+++ static_server/server.js
@@ -6,18 +6,19 @@ const fs = require('fs');
 const server = http.createServer((request, response) => {
 
     let { pathname } = new URL(request.url, 'http://127.0.0.1');
-
+    console.log(pathname.toString());
+    
     let filepath = __dirname + '/page' + pathname;
-    fs.readFile(filepath, (err, data) => {
-        if (err) {
-            response.statusCode = 404;
-            response.end('error');
-            return;
-        }
 
+    fs.readFile(filepath, (err, data) => {
+        if(err){
+             response.statusCode = 500;
+             response.end('not found ');
+             return;
+        }
+        console.log(data.toString());
         response.end(data);
     })
-
 
 });
 
@@ -25,3 +26,4 @@ const server = http.createServer((request, response) => {
 server.listen(9000, () => { // now port  9000 is already used by this node js application
     console.log('server started ...')
 });
+
