54cc823698a25a667539617f95d917afda6162da
charline2020 <p78071044@gs.ncku.edu.tw>
2024-03-21 09:47:19 +0800

    app.method practice

--- express/practice1.js
+++ express/practice1.js
@@ -5,11 +5,28 @@ const express = require('express');
 const app = express();
 
 // create router
-app.get('/home', (req, res)=>{
+app.get('/home', (req, res) => {
     res.end('hello express');
-})
+});
+
+app.get('/', (req, res) => {
+    res.end('home page');
+});
+
+app.post('/login', (req, res)=>{
+    res.end('log in page');
+});
+
+// all methods will response test page
+app.all('/test', (req, res)=>{
+    res.end('test page');
+});
+
+app.all('*', (req, res)=>{
+    res.end('404 not found');
+});
 
 // listen port and start server
-app.listen(9000, ()=>{
+app.listen(9000, () => {
     console.log('server started, port  3000 listening... ')
 })
