12314bd05a49015c6cf4d56ced22a306dd9b8269
charline2020 <p78071044@gs.ncku.edu.tw>
2024-04-01 15:01:48 +0800

    process upload document

--- ejs/generator/routes/index.js
+++ ejs/generator/routes/index.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var { formidable } = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // show website table list
-router.get('/portrait', (req, res)=>{
+router.get('/portrait', (req, res) => {
   res.render('portrait');
 });
 
 // address document upload
-router.post('/portrait', (req, res)=>{
-  res.end('success');
+// files are usually save in website root folder => public
+
+router.post('/portrait', (req, res) => {
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + '/../public/images',
+    keepExtensions: true
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // console.log(fields); // save data like text checkbox select
+    // console.log(files); // save data like file 
+
+    // server save request url for accessing images in the future
+    let url = '/images/' + files.portrait[0].newFilename;
+    // console.log(url);
+
+    // res.json({ fields, files });
+
+    res.send(url);
+    // res.send('okay');
+
+  });
 });
 
 module.exports = router;
