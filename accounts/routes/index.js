2ad8a1057eb7f7a828a359e4f5191b2098435a81
charline2020 <p78071044@gs.ncku.edu.tw>
2024-05-07 10:51:25 -0500

    get code from github: https://github.com/chenyl8848/node.js-study/tree/master/8-%E5%AE%9E%E6%88%98%E6%A1%88%E4%BE%8B

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -4,13 +4,20 @@ var router = express.Router();
 /* list of accounts */
 router.get('/account', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('account list');
+  // res.send('account list');
+  res.render('list'); // views -> list.ejs
 });
 
 /* create account  */
 router.get('/account/create', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('account create');
+  // res.send('account create');
+  res.render('create');
+});
+
+router.post('/account', (req,res) =>{
+  console.log(req.body)
+  res.send('add record');
 });
 
 module.exports = router;
