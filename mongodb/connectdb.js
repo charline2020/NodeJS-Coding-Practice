02825e3364f7fafe2f1b577361e2903054fe45be
charline2020 <p78071044@gs.ncku.edu.tw>
2024-05-09 13:20:50 -0500

    connection use once not on

--- mongodb/connectdb.js
+++ mongodb/connectdb.js
@@ -11,14 +11,14 @@ mongoose.set('strictQuery', true)
 mongoose.connect('mongodb://127.0.0.1:27017')
 
 // 4.set return msg
-mongoose.connection.on('open', () => {
-    console.log('連接成功')
-})
+// mongoose.connection.on('open', () => {
+//     console.log('連接成功')
+// })
 
 // once 事件回调函数只调用一次
-// mongoose.connection.once('open', () => {
-//     console.log('连接成功')
-// })
+mongoose.connection.once('open', () => {
+    console.log('連接成功')
+})
 
 // 5.set connection 錯誤
 mongoose.connection.on('error', () => {
